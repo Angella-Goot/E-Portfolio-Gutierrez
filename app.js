a15357e3d1e93dd7e7b49f6dac9174572409111c
Angella-Goot <angella.gutierrez01@gmail.com>
2026-07-05 16:44:41 -0500

    FEAT/DOCS: EmailJS fully integrated

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-// template_ta9efqm
-// service_w9zu35k
-// 3LSqfaX-jjKMHDjVX <--- public ID
-
+ 
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_w9zu35k',
+            'template_v5i060a',
+          event.target,
+            'hE3oyvOEhYM4R9MXG'
+    ).then (() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+         loading.classList.remove("modal__overlay--visible")
+         alert(
+            "The email service is temporarily unavailable. Please contact me directly on angella.gutierrez01@gmail.com "
+         )
+    })
+}
